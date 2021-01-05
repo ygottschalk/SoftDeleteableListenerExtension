996e31be8d6d811a91663c075a51950de9b5c3d1
Yannick Gottschalk <ygottschalk@techfak.uni-bielefeld.de>
2021-01-05 12:03:06 +0100

    removed symfony bundle stuff, added static AnnotationLoader

---
+++ Loader/AnnotationLoader.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Evence\Bundle\SoftDeleteableExtensionBundle\EventListener;
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+class AnnotationLoader
+{
+    public static function registerAnnotations(): void
+    {
+        AnnotationRegistry::registerFile(
+            dirname(__FILE__, 2) . '/Mapping/Annotation/onSoftDelete.php'
+        );
+        AnnotationRegistry::registerFile(
+            dirname(__FILE__, 2) . '/Mapping/Annotation/onSoftDeleteSuccessor.php'
+        );
+    }
+}
